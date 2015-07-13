c478b3eaff886bf61d97c29c3b1e7949b9c969a9
liuming <liuming0430@gmail.com>
2015-07-13 09:30:14 +0800

    modify syn_queue

--- src/common/syn_queue.c
+++ src/common/syn_queue.c
@@ -8,6 +8,36 @@
 #include "syn_queue.h"
 #include "zmalloc.h"
 
+/*---------------Private Declaration---------------*/
+static void syn_queue_push(syn_queue_t* syn_queue, void* element);
+static void syn_queue_pop(syn_queue_t* syn_queue, void* element);
+
+/*---------------Private Implementation---------------*/
+static void syn_queue_push(syn_queue_t* syn_queue, void* element)
+{
+	pthread_mutex_lock(syn_queue->queue_mutex);
+	while(syn_queue->queue->basic_queue_op->is_full(syn_queue->queue))
+	{
+		pthread_cond_wait(syn_queue->no_full_cond, syn_queue->queue_mutex);
+	}
+	syn_queue->queue->basic_queue_op->push(syn_queue->queue, element);
+	pthread_cond_signal(syn_queue->no_empty_cond);
+	pthread_mutex_unlock(syn_queue->queue_mutex);
+}
+
+static void syn_queue_pop(syn_queue_t* syn_queue, void* element)
+{
+	pthread_mutex_lock(syn_queue->queue_mutex);
+	while(syn_queue->queue->basic_queue_op->is_empty(syn_queue->queue))
+	{
+		pthread_cond_wait(syn_queue->no_empty_cond, syn_queue->queue_mutex);
+	}
+	syn_queue->queue->basic_queue_op->pop(syn_queue->queue, element);
+	pthread_cond_signal(syn_queue->no_full_cond);
+	pthread_mutex_unlock(syn_queue->queue_mutex);
+}
+
+/*-------------------API Implementation--------------------*/
 syn_queue_t* alloc_syn_queue(size_t size, int type_size)
 {
 	syn_queue_t* syn_queue = (syn_queue_t*)zmalloc(sizeof(syn_queue_t));
@@ -15,10 +45,13 @@ syn_queue_t* alloc_syn_queue(size_t size, int type_size)
 	syn_queue->queue_mutex = (pthread_mutex_t*)zmalloc(sizeof(pthread_mutex_t));
 	syn_queue->no_empty_cond = (pthread_cond_t*)zmalloc(sizeof(pthread_cond_t));
 	syn_queue->no_full_cond = (pthread_cond_t*)zmalloc(sizeof(pthread_cond_t));
+	syn_queue->op = zmalloc(sizeof(syn_queue_op_t));
 	pthread_mutex_init(syn_queue->queue_mutex, NULL);
 	pthread_cond_init(syn_queue->no_empty_cond, NULL);
 	pthread_cond_init(syn_queue->no_full_cond, NULL);
 
+	syn_queue->op->syn_queue_push = syn_queue_push;
+	syn_queue->op->syn_queue_pop = syn_queue_pop;
 	return syn_queue;
 }
 
@@ -37,30 +70,6 @@ void destroy_syn_queue(syn_queue_t* syn_queue)
 	zfree(syn_queue->no_empty_cond);
 	zfree(syn_queue->queue_mutex);
 	zfree(syn_queue);
-}
-
-void syn_queue_push(syn_queue_t* syn_queue, void* element)
-{
-	pthread_mutex_lock(syn_queue->queue_mutex);
-	while(syn_queue->queue->basic_queue_op->is_full(syn_queue->queue))
-	{
-		pthread_cond_wait(syn_queue->no_full_cond, syn_queue->queue_mutex);
-	}
-	syn_queue->queue->basic_queue_op->push(syn_queue->queue, element);
-	pthread_cond_signal(syn_queue->no_empty_cond);
-	pthread_mutex_unlock(syn_queue->queue_mutex);
-}
-
-void syn_queue_pop(syn_queue_t* syn_queue, void* element)
-{
-	pthread_mutex_lock(syn_queue->queue_mutex);
-	while(syn_queue->queue->basic_queue_op->is_empty(syn_queue->queue))
-	{
-		pthread_cond_wait(syn_queue->no_empty_cond, syn_queue->queue_mutex);
-	}
-	syn_queue->queue->basic_queue_op->pop(syn_queue->queue, element);
-	pthread_cond_signal(syn_queue->no_full_cond);
-	pthread_mutex_unlock(syn_queue->queue_mutex);
 }
 
 #define SYN_QUEUE_TEST 1
--- src/common/syn_queue.h
+++ src/common/syn_queue.h
@@ -9,20 +9,24 @@
 #include <pthread.h>
 #include "basic_queue.h"
 
+struct syn_queue_op {
+	void (*syn_queue_push)(struct syn_queue *syn_queue, void *element);
+	void (*syn_queue_pop)(struct syn_queue *syn_queue, void *element);
+};
+
 struct syn_queue
 {
 	basic_queue_t *queue;
 	pthread_mutex_t *queue_mutex;
 	pthread_cond_t *no_empty_cond;
 	pthread_cond_t *no_full_cond;
+	struct syn_queue_op *op;
 };
 
 typedef struct syn_queue syn_queue_t;
+typedef struct syn_queue_op syn_queue_op_t;
 
 syn_queue_t* alloc_syn_queue(size_t size, int type_size);
 void destroy_syn_queue(syn_queue_t *);
-void syn_queue_push(syn_queue_t*, void*);
-void syn_queue_pop(syn_queue_t*, void*);
-
 
 #endif
