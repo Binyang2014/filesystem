5380e821a38708b6488a3c689e6486fee9c2b562
liuming <liuming0430@gmail.com>
2015-07-15 19:22:56 +0800

    name space

--- src/common/name_space.c
+++ src/common/name_space.c
@@ -10,6 +10,23 @@
 #include <stdlib.h>
 #include "name_space.h"
 
+/*-------------------Private Implementation--------------------*/
+static void *list_dup(void *ptr) {
+	pair_t *pair = zmalloc(sizeof(*pair));
+	pair_t *tmp = ptr;
+	pair->key = sds_dup(tmp->key);
+	file_node_t *node = zmalloc(sizeof(*node));
+	memcpy(node, tmp->value, sizeof(*node));
+	pair->value = node;
+	return pair;
+}
+
+static void list_free(void *ptr) {
+	pair_t *pair = ptr;
+	sds_free(pair->key);
+	zfree(pair->value);
+}
+
 static file_node_t *find(name_space_t *space, sds name) {
 	return space->name_space->op->get(space->name_space, name);
 }
@@ -17,14 +34,15 @@ static file_node_t *find(name_space_t *space, sds name) {
 static int add_file(name_space_t *space, sds file_name, enum file_type_enum type) {
 	file_node_t *node = find(space, file_name);
 	if(node != NULL) {
-		return 00;//TODO
+		return -1;//TODO
 	}
 
 	node = zmalloc(sizeof(file_node_t));
 	sds_cpy(node->file_name, file_name);
 	node->file_size = 0;
 	node->file_type = type;
-
+	node->consistent_size = 0;
+	space->file_num++;
 	return 1;
 }
 
@@ -40,18 +58,52 @@ static int rename_file(name_space_t *space, sds old_name, sds new_name) {
 	return space->name_space->op->modify_key(space->name_space, old_name, new_name);
 }
 
-static file_node_t *get_file_node(name_space_t *space, sds file_name) {
-	return space->name_space->op->get(space->name_space, file_name);;
-}
-
-int delete_file(name_space_t *space, sds file_name) {
+static int delete_file(name_space_t *space, sds file_name) {
 	return space->name_space->op->del(space->name_space, file_name);
 }
 
-name_space_t *create_name_space(size_t size) {
+static int file_finish_consistent(struct name_space *space, sds file_name) {
+	file_node_t *node = find(space, file_name);
 
+	if(node == NULL) {
+		return -1;
+	}
+
+	return node->consistent_size == node->file_size;
+}
+
+static int file_exists(struct name_space *space, sds file_name) {
+	return find(space, file_name) != NULL;
+}
+
+
+/*--------------------API Implementation-------------------*/
+name_space_t *create_name_space(size_t size) {
+	name_space_t *this = zmalloc(sizeof(*this));
+
+	this->name_space = create_map(size, NULL, NULL, list_dup, list_free);
+	this->file_num = 0;
+	this->op = zmalloc(sizeof(name_space_op_t));
+	this->op->add_persistent_file = add_persistent_file;
+	this->op->add_temporary_file =  add_temporary_file;
+	this->op->delete_file = delete_file;
+	this->op->file_finish_consistent = file_finish_consistent;
+	this->op->rename_file = rename_file;
+
+	return this;
 }
 
 void destroy_name_space(name_space_t *this) {
+	zfree(this->op);
+	destroy_map(this->name_space);
+	zfree(this);
+}
+
+
+/*------------------T	E	S	T-----------------*/
+
+#if defined(GLOBAL_TEST) || defined(NAME_SPACE_TEST)
+int main() {
 
 }
+#endif
--- src/common/name_space.h
+++ src/common/name_space.h
@@ -24,6 +24,7 @@ struct name_space_file_location {
 struct file_node {
 	uint8_t file_type; //{0:persistent, 1:temporary}
 	sds file_name;
+	uint64_t consistent_size;
 	uint64_t file_size;
 	struct name_space_file_location *location;
 
@@ -33,8 +34,9 @@ struct name_space_op {
 	int (*add_temporary_file)(struct name_space space, sds file_name);
 	int (*add_persistent_file)(struct name_space space, sds file_name);
 	int (*rename_file)(struct name_space space, sds old_name, sds new_name);
-	struct file_ndoe *(*get_file_node)(struct name_space space, sds file_name);
 	int (*delete_file)(struct name_space space, sds file_name);
+	int (*file_finish_consistent)(struct name_space *space, sds file_name);
+	int (*file_exists)(struct name_space *space, sds file_name);
 };
 
 struct name_space {
@@ -44,6 +46,7 @@ struct name_space {
 };
 
 typedef struct name_space name_space_t;
+typedef struct name_space_op name_space_op_t;
 typedef struct file_node file_node_t;
 typedef struct name_space_file_location n_s_f_l_t;
 
