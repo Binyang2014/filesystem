9e52e263b593151b75870ea4c5e69c8fa7895a25
liuming <liuming0430@gmail.com>
2015-07-22 17:04:38 +0800

    submaster

--- src/sub-master/sub_master.c
+++ src/sub-master/sub_master.c
@@ -8,14 +8,31 @@
 #include "sub_master.h"
 
 /*===============Private Declaration===============*/
+static sub_master_t *local_this;
 
 static void read_temp_file();
 //static void
 
 /*---------------------Sub-Master RPC Service------------------*/
-static void allocate_temp_file_space();
-static void create_temp_file();
-static void delete_temp_file();
+static void *get_event_handler_param(event_handler_t *event_handler) {
+	return event_handler->event_buffer_list->head->value;
+}
+
+static void allocate_temp_file_space(event_handler_t* event_handler) {
+
+}
+
+static void create_temp_file(event_handler_t* event_handler) {
+
+}
+
+static void delete_temp_file(event_handler_t* event_handler) {
+
+}
+
+static void append_tmp_file(event_handler_t* event_handler) {
+
+}
 
 
 /**
@@ -33,3 +50,23 @@ static void sub_master_server() {
 
 }
 
+static void *resolve_handler(event_handler_t* event_handler, void* msg_queue) {
+	common_msg_t common_msg;
+	syn_queue_t* queue = msg_queue;
+	queue->op->syn_queue_pop(queue, &common_msg);
+	switch(common_msg.operation_code)
+	{
+		default:
+			event_handler->handler = NULL;
+	}
+	return event_handler->handler;
+}
+
+
+sub_master_t *create_sub_master(int rank, machine_role_t *role) {
+	sub_master_t *this = zmalloc(sizeof(*this));
+	local_this = this;
+	this->role = role;
+	this->rpc_server = create_mpi_rpc_server(2, rank, resolve_handler);
+	return this;
+}
--- src/sub-master/sub_master.h
+++ src/sub-master/sub_master.h
@@ -7,16 +7,33 @@
 
 #ifndef SRC_SUB_MASTER_SUB_MASTER_H_
 #define SRC_SUB_MASTER_SUB_MASTER_H_
-
+#include <stdint.h>
+#include <pthread.h>
 #include "../common/name_space.h"
 #include "../common/sds.h"
 #include "../common/communication/mpi_rpc_server.h"
 #include "../common/communication/mpi_rpc_client.h"
 
-typedef struct {
+struct data_server_des {
+
+};
+
+struct sub_master_op {
+
+};
+
+struct sub_master{
+	machine_role_t *role;
 	name_space_t *name_space;
 	mpi_rpc_server_t *rpc_server;
 	mpi_rpc_client_t *rpc_client;
+	uint64_t sub_global_id;
+	pthread_mutex_t mutex_id;
 };
+
+typedef struct data_server_des data_server_des_t;
+typedef struct sub_master sub_master_t;
+typedef struct data_server_des data_server_des_t;
+
 
 #endif /* SRC_SUB_MASTER_SUB_MASTER_H_ */
