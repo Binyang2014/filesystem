3ddbebaf5851367f1c6a525b008ac4a38d5053db
binyang2014 <bin.yang.li.2010@gmail.com>
2015-07-23 15:18:09 +0800

    change something about message structure

---
+++ src/data-server/server/dataserver_buff.c
@@ -0,0 +1,328 @@
+/**
+ * created on 2015.4.16
+ * author: Binyang
+ * buffer handler functions
+ */
+#include <string.h>
+#include <stdlib.h>
+#include "dataserver_buff.h"
+#include "../../tool/errinfo.h"
+#include "../../structure/bitmap.h"
+#include "../../structure/basic_list.h"
+#include "../../structure/basic_queue.h"
+
+//need to be init, I will do it later
+static list_node_t* list_node_arr;
+static msg_data_t* msg_data_arr;
+static common_msg_t* common_msg_arr;
+static dataserver_file_t* file_arr;
+static void* reply_message_arr;
+static vfs_hashtable_t* f_map_arr;
+static vfs_hashtable_t summary_table;
+
+/*===================== Prototypes ==========================*/
+static void buff_node_dup(void*, void*);
+static void reply_msg_dup(void*, void*);
+static void m_data_dup(void*, void*);
+static void file_info_dup(void*, void*);
+static void f_arr_dup(void*, void*);
+static void common_msg_buff_dup(void*, void*);
+
+/*===================== Interface Implement ================*/
+/**
+ * get the list of buffer, if error return NULL
+ */
+list_t* get_buffer_list(data_server_t* data_server, int num)
+{
+	int i;
+	list_t* list;
+	list_node_t* node = NULL;
+	basic_queue_t* buff_queue;
+	if((list = list_create()) == NULL)
+		return NULL;
+	buff_queue = data_server->buff_node_queue;
+
+	for(i = 0; i < num; i++)
+	{
+		buff_queue->basic_queue_op->pop(buff_queue, &node);
+		list->list_ops->list_add_exist_node_tail(list, node);
+	}
+	return list;
+}
+
+/**
+ * get a block of message buffer from data server
+ */
+void* get_reply_msg_buff(data_server_t* data_server)
+{
+	void* msg_buffer = NULL;
+	basic_queue_t* buff_queue;
+	buff_queue = data_server->reply_message_buff;
+
+	buff_queue->basic_queue_op->pop(buff_queue, &msg_buffer);
+	return msg_buffer;
+}
+
+void* get_data_buff(data_server_t* data_server)
+{
+	void* data_buffer = NULL;
+	basic_queue_t* buff_queue;
+	buff_queue = data_server->m_data_buff;
+
+	buff_queue->basic_queue_op->pop(buff_queue, &data_buffer);
+	return data_buffer;
+}
+
+void* get_file_info_buff(data_server_t* data_server)
+{
+	void* file_info_buffer = NULL;
+	basic_queue_t* buff_queue;
+	buff_queue = data_server->file_buff;
+
+	buff_queue->basic_queue_op->pop(buff_queue, &file_info_buffer);
+	return file_info_buffer;
+}
+
+void* get_common_msg_buff(data_server_t* data_server)
+{
+	void* common_msg = NULL;
+	basic_queue_t* buff_queue;
+	buff_queue = data_server->common_msg_buff;
+
+	buff_queue->basic_queue_op->pop(buff_queue, &common_msg);
+	return common_msg;
+}
+
+/*
+ * get the number of buffers, because this is a array maybe I need
+ * to reorganize it. How to get a series of room in memory. Maybe
+ * use bitmap?
+ */
+void* get_f_arr_buff(data_server_t* data_server, int num)
+{
+	vfs_hashtable_t* f_arr_buffer = NULL;
+	basic_queue_t* buff_queue;
+	unsigned long pos;
+
+	buff_queue = data_server->f_arr_buff;
+
+	buff_queue->basic_queue_op->pop(buff_queue, &f_arr_buffer);
+
+	f_arr_buffer->hash_table_size = num;
+	pos = bitmap_find_next_zero_area(data_server->f_arr_bitmap, F_ARR_SIZE, 0, num, 0);
+
+#ifdef DATASERVER_BUFF_DEBUG
+	printf("The start position is %ld\n", pos);
+#endif
+
+	//if pos == size, do something, maybe realloc
+	f_arr_buffer->blocks_arr = &summary_table.blocks_arr[pos];
+	f_arr_buffer->chunks_arr = &summary_table.chunks_arr[pos];
+#ifdef DATASERVER_BUFF_DEBUG
+	printf("The blocks buffer address is %p\n", &summary_table.blocks_arr[pos]);
+#endif
+	bitmap_set(data_server->f_arr_bitmap, pos, num);
+	return f_arr_buffer;
+}
+
+/**
+ * return a block of message buffer to data server
+ */
+void reture_common_msg_buff(data_server_t* data_server, void* buff)
+{
+	basic_queue_t* buff_queue;
+	buff_queue = data_server->common_msg_buff;
+	buff_queue->basic_queue_op->push(buff_queue, &buff);
+}
+
+void return_reply_msg_buff(data_server_t* data_server, void* buff)
+{
+	basic_queue_t* buff_queue;
+	buff_queue = data_server->reply_message_buff;
+	buff_queue->basic_queue_op->push(buff_queue, &buff);
+}
+
+void return_data_buff(data_server_t* data_server, void* buff)
+{
+	basic_queue_t* buff_queue;
+	buff_queue = data_server->m_data_buff;
+	buff_queue->basic_queue_op->push(buff_queue, &buff);
+}
+
+void return_file_info_buff(data_server_t* data_server, void* buff)
+{
+	basic_queue_t* buff_queue;
+	buff_queue = data_server->file_buff;
+	buff_queue->basic_queue_op->push(buff_queue, &buff);
+}
+
+void return_f_arr_buff(data_server_t* data_server, vfs_hashtable_t* buff)
+{
+	basic_queue_t* buff_queue;
+	unsigned long pos;
+	int num;
+
+	buff_queue = data_server->f_arr_buff;
+	buff_queue->basic_queue_op->push(buff_queue, &buff);
+	num = buff->hash_table_size;
+	//Calculate start position
+	pos = ((unsigned long)(buff->blocks_arr) - (unsigned long)(summary_table.blocks_arr)) /
+			sizeof(unsigned int);
+
+#ifdef DATASERVER_BUFF_DEBUG
+	printf("The release position is %ld\n", pos);
+#endif
+
+	bitmap_clear(data_server->f_arr_bitmap, pos, num);
+	buff->hash_table_size = 0;
+	buff->blocks_arr = NULL;
+	buff->chunks_arr = NULL;
+}
+
+//allocate buffer for data server. This function should be run first
+void set_data_server_buff(data_server_t* data_server, int init_length)
+{
+	int i;
+	void* ptr_temp;
+
+	//allocate message queue
+	if((data_server->buff_node_queue = alloc_basic_queue(sizeof(list_node_t*), BUFF_NODE_SIZE + 1))
+			== NULL)
+		err_sys("error when allocate buffer");
+	if((data_server->m_data_buff= alloc_basic_queue(sizeof(msg_data_t*), init_length + 1))
+			== NULL)
+		err_sys("error when allocate buffer");
+	if((data_server->common_msg_buff = alloc_basic_queue(sizeof(common_msg_t*), init_length + 1))
+			== NULL)
+		err_sys("error when allocate buffer");
+	if((data_server->file_buff = alloc_basic_queue(sizeof(dataserver_file_t*), init_length + 1))
+			== NULL)
+		err_sys("error when allocate buffer");
+	if((data_server->reply_message_buff = alloc_basic_queue(sizeof(void*), init_length + 1))
+			== NULL)
+		err_sys("error when allocate buffer");
+	if((data_server->f_arr_buff = alloc_basic_queue(sizeof(vfs_hashtable_t*), init_length + 1))
+			== NULL)
+		err_sys("error when allocate buffer");
+
+	//set dup method
+	queue_set_dup_method(data_server->buff_node_queue, buff_node_dup);
+	queue_set_dup_method(data_server->m_data_buff, m_data_dup);
+	queue_set_dup_method(data_server->common_msg_buff, common_msg_buff_dup);
+	queue_set_dup_method(data_server->file_buff, file_info_dup);
+	queue_set_dup_method(data_server->reply_message_buff, reply_msg_dup);
+	queue_set_dup_method(data_server->f_arr_buff, f_arr_dup);
+
+	if((list_node_arr = (list_node_t* )malloc(sizeof(list_node_t) * init_length *
+			BUFF_NODE_SIZE)) == NULL)
+		err_sys("error when allocate buffer");
+	if((msg_data_arr = (msg_data_t* )malloc(sizeof(msg_data_t) * init_length)) == NULL)
+		err_sys("error when allocate buffer");
+	if((common_msg_arr = (common_msg_t* )malloc(sizeof(common_msg_t) * init_length)) == NULL)
+		err_sys("error when allocate buffer");
+	if((file_arr = (dataserver_file_t* )malloc(sizeof(dataserver_file_t) * init_length))
+			== NULL)
+		err_sys("error when allocate buffer");
+	if((reply_message_arr = (void* )malloc(MAX_CMD_MSG_LEN * init_length)) == NULL)
+		err_sys("error when allocate buffer");
+	if((f_map_arr = (vfs_hashtable_t* )malloc(sizeof(vfs_hashtable_t) * F_ARR_SIZE)) == NULL)
+		err_sys("error when allocate buffer");
+
+	//allocate for data server array map structure
+	summary_table.hash_table_size = F_ARR_SIZE;
+	if((summary_table.blocks_arr = (unsigned int* )malloc(sizeof(unsigned int)	* F_ARR_SIZE))
+			== NULL)
+		err_sys("error when allocate buffer");
+	if((summary_table.chunks_arr = (unsigned long long* )malloc(sizeof(unsigned long long)*
+			F_ARR_SIZE)) == NULL)
+		err_sys("error when allocate buffer");
+	if((data_server->f_arr_bitmap = (unsigned long*)malloc(sizeof(unsigned long) *
+			F_ARR_SIZE / BITS_PER_LONG)) == NULL)
+		err_sys("error when allocate buffer");
+
+	//initial data server buffer by set value
+	for(i = 0; i < init_length; i++)
+	{
+		ptr_temp = &msg_data_arr[i];
+		data_server->m_data_buff->basic_queue_op->push(data_server->m_data_buff,
+				&ptr_temp);
+
+		ptr_temp = &common_msg_arr[i];
+		data_server->common_msg_buff->basic_queue_op->push(data_server->common_msg_buff,
+				&ptr_temp);
+
+		ptr_temp = &file_arr[i];
+		data_server->file_buff->basic_queue_op->push(data_server->file_buff,
+				&ptr_temp);
+
+		ptr_temp = reply_message_arr + MAX_CMD_MSG_LEN * i;
+		data_server->reply_message_buff->basic_queue_op->push(data_server->reply_message_buff,
+				&ptr_temp);
+
+		ptr_temp = &f_map_arr[i];
+		data_server->f_arr_buff->basic_queue_op->push(data_server->f_arr_buff,
+				&ptr_temp);
+	}
+	for(i = 0; i < BUFF_NODE_SIZE; i++)
+	{
+		ptr_temp = &list_node_arr[i];
+		list_node_arr[i].value = NULL;
+		list_node_arr[i].next = NULL;
+		list_node_arr[i].prev = NULL;
+		data_server->buff_node_queue->basic_queue_op->push(data_server->buff_node_queue,
+				&ptr_temp);
+	}
+}
+
+void return_buffer_list(data_server_t* data_server, list_t* list)
+{
+	int i, num;
+	list_node_t* node;
+	basic_queue_t* buff_queue;
+	list_iter_t *iter;
+
+	num = list->len;
+	buff_queue = data_server->buff_node_queue;
+
+	iter = list->list_ops->list_get_iterator(list, AL_START_HEAD);
+	for(i = 0; i < num; i++)
+	{
+		node = list->list_ops->list_next(iter);
+		buff_queue->basic_queue_op->push(buff_queue, &node);
+	}
+	list->list_ops->list_release_iterator(iter);
+	list_release_without_node(list);
+}
+
+/*===================== Static Functions======================*/
+/**
+ * just return itself and do not allocate new room for the node
+ */
+static void buff_node_dup(void* dest, void* buff_node)
+{
+	*(void** )dest = *(void** )buff_node;
+}
+
+static void reply_msg_dup(void* dest, void* reply_msg)
+{
+	*(void** )dest =  *(void** )reply_msg;
+}
+
+static void m_data_dup(void* dest, void* m_data)
+{
+	*(void** )dest = *(void** )m_data;
+}
+static void file_info_dup(void* dest, void* file_info)
+{
+	*(void** )dest = *(void** )file_info;
+}
+
+static void f_arr_dup(void* dest, void* f_arr)
+{
+	*(void** )dest = *(void** )f_arr;
+}
+
+static void common_msg_buff_dup(void* dest, void* common_msg)
+{
+	*(void** )dest = *(void** )common_msg;
+}
---
+++ src/data-server/server/dataserver_handler.c
@@ -0,0 +1,316 @@
+/*
+ * created on 2015.4.7
+ * author: Binyang
+ *
+ * This file complete functions defined in dastaserver_comm.h
+ */
+#include <string.h>
+#include <mpi.h>
+#include <stdlib.h>
+#include "dataserver_handler.h"
+#include "dataserver_buff.h"
+#include "../../tool/errinfo.h"
+#include "../structure/vfs_structure.h"
+#include "../../tool/message.h"
+#include "../../tool/threadpool.h"
+
+/*=========================INTERNEL FUNCITION EVOKED BY HANDLER================================*/
+static int read_from_vfs(dataserver_file_t *file, msg_data_t* buff, size_t count,
+		off_t offset, int seqno, int tail)
+{
+	int ans;
+	char* data_buff = (char*)buff->data;
+
+	//read a block from vfs
+	if((ans = vfs_read(file, data_buff, count, offset)) == -1)
+	{
+		err_msg("Something wrong when read from data server");
+		return -1;
+	}
+
+
+	buff->len = count;
+	buff->seqno = seqno;
+	buff->offset = offset + ans;//I don't know if this operation is right
+
+	//read to the end of the file unexpectedly
+	if(ans < count)
+		buff->tail = 1;
+	buff->tail = tail;
+	return ans;
+}
+
+/**
+ * handle the request from client about reading from a file
+ * buff and msg_buff should be allocated properly outside
+ */
+ static int m_read_handler(int source, int tag, msg_for_rw_t* file_info, void* buff,
+		 void* msg_buff)
+{
+	int ans = 0, msg_blocks, msg_rest, i, temp_ans = 0;
+	off_t offset;
+	mpi_status_t status;
+
+#ifdef DATASERVER_COMM_DEBUG
+	printf("start read from data server\n");
+#endif
+
+	d_mpi_acc_recv(msg_buff, source, tag, &status);
+	//client do not want to receive message, and server will not provide
+	if(*(unsigned short*)msg_buff != MSG_ACC)
+	{
+		err_msg("The client do not ready to receive message now");
+		return -1;
+	}
+
+	msg_blocks = file_info->count / MAX_DATA_CONTENT_LEN;
+	msg_rest = file_info->count % MAX_DATA_CONTENT_LEN;
+	offset = file_info->offset;
+	msg_blocks = msg_blocks + (msg_rest ? 1 : 0);
+
+#ifdef DATASERVER_COMM_DEBUG
+	printf("The acc in read handler\n");
+	printf_msg_status(&status);
+	printf("send message to client\n");
+#endif
+
+	for(i = 0; i < msg_blocks - 1; i++)
+	{
+		if( (temp_ans = read_from_vfs(file_info->file, buff, MAX_DATA_CONTENT_LEN,
+				offset, i, 0)) == -1 )
+		{
+			//here thread should send error message to client
+			return -1;
+		}
+
+		//send message to client
+		d_mpi_data_send(buff, source, tag);
+
+		//already read at the end of the file
+		if(temp_ans == 0)
+			return ans;
+
+		offset = offset + temp_ans;
+		ans = ans + temp_ans;
+	}
+
+	//send tail message
+	if(msg_rest == 0)
+	{
+		if( (temp_ans = read_from_vfs(file_info->file, buff, MAX_DATA_CONTENT_LEN,
+						offset, i, 1)) == -1 )
+			return -1;
+	}
+	else
+	{	if( (temp_ans = read_from_vfs(file_info->file, buff, msg_rest,
+						offset, i, 1)) == -1 )
+			return -1;
+	}
+
+	d_mpi_data_send(buff, source, tag);
+	ans = ans + temp_ans;
+
+#ifdef DATASERVER_COMM_DEBUG
+	printf("read finished and the bytes of read is %d\n", ans);
+#endif
+
+	//maybe there is need to receive Acc from client
+	return ans;
+}
+
+//handle the request from client about writing to a file
+static int write_to_vfs(dataserver_file_t *file, msg_data_t* buff, off_t offset)
+{
+	int ans, len;
+	char* data_buff;
+	len = buff->len;
+	data_buff = (char*)buff->data;
+
+	//if we need seq number to keep message in order??
+	//write to file system
+	if((ans = vfs_write(file, data_buff, len, offset)) == -1)
+	{
+		err_msg("Something wrong when read from data server");
+		return -1;
+	}
+	return ans;
+}
+
+//write message handler
+static int m_write_handler(int source, int tag, msg_for_rw_t* file_info, void* buff,
+		void* msg_buff)
+{
+	printf("source is %d, tag is %d\n", source, tag);
+	int ans = 0, msg_blocks, msg_rest, i, temp_ans = 0;
+	off_t offset;
+	mpi_status_t status;
+
+	((msg_acc_candd_t* )msg_buff)->operation_code = MSG_ACC;
+	((msg_acc_candd_t* )msg_buff)->status = 0;
+
+	d_mpi_cmd_send(msg_buff, source, tag);
+	//memset(msg_buff, 0, MAX_CMD_MSG_LEN);
+
+	msg_blocks = file_info->count / MAX_DATA_CONTENT_LEN;
+	msg_rest = file_info->count % MAX_DATA_CONTENT_LEN;
+	offset = file_info->offset;
+	msg_blocks = msg_blocks + (msg_rest ? 1 : 0);
+
+#ifdef DATASERVER_COMM_DEBUG
+	printf("will receive message from client\n");
+#endif
+
+	for(i = 0; i < msg_blocks; i++)
+	{
+		//received message from client
+		//if we need seq number to keep message in order??
+		d_mpi_data_recv(buff, source, tag, &status);
+
+		if((temp_ans = write_to_vfs(file_info->file, buff, offset)) == -1)
+			return -1;
+
+		offset = offset + temp_ans;
+		ans = ans + temp_ans;
+	}
+
+#ifdef DATASERVER_COMM_DEBUG
+	printf("write finish and the number of write bytes is %d\n", ans);
+	printf_msg_status(&status);
+#endif
+
+	//maybe there is need to send Acc to client
+	return ans;
+}
+
+/*====================================REAL HANDLERS===========================================*/
+
+static void resolve_rw_handler_buffer(event_handler_t* event_handle,
+		rw_handle_buff_t* handle_buff)
+{
+	list_node_t* node;
+	list_t* buffer_list = event_handle->event_buffer_list;
+	list_iter_t* iter = buffer_list->list_ops->list_get_iterator(buffer_list, 0);
+
+	node = buffer_list->list_ops->list_next(iter);
+	handle_buff->common_msg = list_node_value(node);
+
+	node = buffer_list->list_ops->list_next(iter);
+	handle_buff->msg_buffer = list_node_value(node);
+
+	node = buffer_list->list_ops->list_next(iter);
+	handle_buff->data_buffer = list_node_value(node);
+
+	node = buffer_list->list_ops->list_next(iter);
+	handle_buff->file_info->file = list_node_value(node);
+
+	node = buffer_list->list_ops->list_next(iter);
+	handle_buff->f_arr_buff =  list_node_value(node);
+
+	buffer_list->list_ops->list_release_iterator(iter);
+}
+
+static void release_rw_handler_buffer(event_handler_t* event_handle)
+{
+	list_node_t* node;
+	list_t* buffer_list = event_handle->event_buffer_list;
+	list_iter_t* iter = buffer_list->list_ops->list_get_iterator(buffer_list, 0);
+	data_server_t* dataserver = event_handle->spcical_struct;
+	void* value;
+
+	node = buffer_list->list_ops->list_next(iter);
+	value = list_node_value(node);
+	reture_common_msg_buff(dataserver, value);
+
+	node = buffer_list->list_ops->list_next(iter);
+	value = list_node_value(node);
+	return_reply_msg_buff(dataserver, value);
+
+	node = buffer_list->list_ops->list_next(iter);
+	value = list_node_value(node);
+	return_data_buff(dataserver, value);
+
+	node = buffer_list->list_ops->list_next(iter);
+	value = list_node_value(node);
+	return_file_info_buff(dataserver, value);
+
+	node = buffer_list->list_ops->list_next(iter);
+	value = list_node_value(node);
+	return_f_arr_buff(dataserver, value);
+
+	buffer_list->list_ops->list_release_iterator(iter);
+	return_buffer_list(dataserver, buffer_list);
+}
+
+void d_read_handler(event_handler_t* event_handle)
+{
+	int source, tag, i;
+	rw_handle_buff_t handle_buff;
+	msg_r_ctod_t* read_msg;
+	data_server_t* this;
+
+	//allocate file_info this structure do not need buffer
+	handle_buff.file_info = (msg_for_rw_t*)malloc(sizeof(msg_for_rw_t));
+	resolve_rw_handler_buffer(event_handle, &handle_buff);
+	//Data server should be the special structure. It is not a buffer
+	this = event_handle->spcical_struct;
+
+	//initial basic information from handle_buffer and event_handle
+	read_msg = (msg_r_ctod_t* )MSG_COMM_TO_CMD(handle_buff.common_msg);
+	source = handle_buff.common_msg->source;
+	tag = read_msg->unique_tag;
+
+	handle_buff.file_info->offset = read_msg->offset;
+	handle_buff.file_info->count = read_msg->read_len;
+	handle_buff.f_arr_buff->hash_table_size = read_msg->chunks_count;
+	for(i = 0; i < read_msg->chunks_count; i++)
+		handle_buff.f_arr_buff->chunks_arr[i] = read_msg->chunks_id_arr[i];
+	handle_buff.file_info->file = init_vfs_file(this->d_super_block, handle_buff.file_info->file,
+			handle_buff.f_arr_buff, VFS_READ);
+
+
+	if(m_read_handler(source, tag, handle_buff.file_info, handle_buff.data_buffer, handle_buff.msg_buffer) == -1)
+	{
+		//do something like sending error message to client
+		//release_rw_handler_buffer(event_handle, &handle_buff);
+	}
+	release_rw_handler_buffer(event_handle);
+	free(handle_buff.file_info);
+	printf("It's OK here\n");
+}
+
+void d_write_handler(event_handler_t* event_handle)
+{
+	int source, tag, i;
+	msg_w_ctod_t* write_msg;
+	data_server_t* this;
+	rw_handle_buff_t handle_buff;
+
+	//allocate file_info this structure do not need buffer
+	handle_buff.file_info = (msg_for_rw_t*)malloc(sizeof(msg_for_rw_t));
+	resolve_rw_handler_buffer(event_handle, &handle_buff);
+	//Data server should be the special structure. It is not a buffer
+	this = event_handle->spcical_struct;
+
+	//init basic information, and it just for test now!!
+	write_msg = (msg_w_ctod_t* )MSG_COMM_TO_CMD(handle_buff.common_msg);
+	source = handle_buff.common_msg->source;
+	//tag = common_msg->unique_tag;
+	tag = write_msg->unique_tag;
+
+	handle_buff.file_info->offset = write_msg->offset;
+	handle_buff.file_info->count = write_msg->write_len;
+	handle_buff.f_arr_buff->hash_table_size = write_msg->chunks_count;
+	for(i = 0; i < write_msg->chunks_count; i++)
+		handle_buff.f_arr_buff->chunks_arr[i] = write_msg->chunks_id_arr[i];
+	handle_buff.file_info->file = init_vfs_file(this->d_super_block, handle_buff.file_info->file,
+			handle_buff.f_arr_buff, VFS_WRITE);
+
+	if(m_write_handler(source, tag, handle_buff.file_info, handle_buff.data_buffer,
+			handle_buff.msg_buffer) == -1)
+	{
+		//do somthing here
+		//release_rw_handler_buffer(event_handle, &handle_buff);
+	}
+	release_rw_handler_buffer(event_handle);
+	free(handle_buff.file_info);
+}
