4be59df8cd1da6296aba369a773f8521bd232361
binyang2014 <bin.yang.li.2010@gmail.com>
2015-07-23 15:24:13 +0800

    remove useless files

--- old/src/tool/test/hash_test.d
+++
@@ -1,1 +0,0 @@
-hash_test.o hash_test.d: hash_test.c ../hash.h
--- old/src/tool/test/syn_tool_test.d
+++
@@ -1,4 +0,0 @@
-syn_tool_test.o syn_tool_test.d: syn_tool_test.c ../syn_tool.h \
- ../../structure/basic_queue.h ../message.h /usr/include/mpich/mpi.h \
- /usr/include/mpich/mpio.h /usr/include/mpich/mpi.h ../../global.h \
- ../../structure/basic_queue.h
