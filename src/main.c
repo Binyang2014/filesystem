0d0f9141a07570959a12f5a4c16b6c43cfb53d09
liuming <liuming0430@gmail.com>
2015-12-29 16:17:53 +0800

    modify main

--- src/main.c
+++ src/main.c
@@ -24,7 +24,7 @@ static c_d_register_t* get_register_cmd(int rank, uint64_t freeblock, int tag, c
 	c_d_register_t *re = zmalloc(sizeof(*re));
 	re->free_block =  (1 << freeblock) / BLOCK_SIZE - 8;
 	re->source = rank;
-	get_visual_ip(net_name, re->ip);
+	//get_visual_ip(net_name, re->ip);
 	re->operation_code = REGISTER_TO_DATA_MASTER_CODE;
 	re->unique_tag = tag;
 	return re;
@@ -41,36 +41,35 @@ int main(argc, argv)
 	pthread_t *thread_data_master = (pthread_t *)zmalloc(sizeof(*thread_data_master));
 	pthread_t *thread_data_server = (pthread_t *)zmalloc(sizeof(*thread_data_master));
 	pthread_t *thread_client = (pthread_t *)zmalloc(sizeof(*thread_data_master));
-	map_role_value_t *map_role = NULL;
+	map_role_value_t *map_role =  zmalloc(sizeof(*map_role));
 	mpi_init_with_thread(&argc, &argv);
 	rank = get_mpi_rank();
 	size = get_mpi_size();
 
 	log_init("", LOG_TRACE);
-
-	if (rank == 0) 
-	{
-		map_role = machine_role_allocator_start(size - 1, rank, file_path, net_name);
-	}else
-	{
-		usleep(50);
-		map_role = get_role(rank, net_name);
-	}
-	usleep(10);
 	log_write(LOG_DEBUG, "get role success and role id is %d role ip is %s ", map_role->rank, map_role->ip);
 
-	if (map_role->type == DATA_MASTER) {
+	if (rank == 0) {
+		map_role->type = DATA_MASTER;
+		map_role->master_rank = 0;
+		map_role->group_size = size - 1;
+		map_role->rank = 0;
 		log_write(LOG_DEBUG, "ROLE DATA_MASTER AND ID IS %d", rank);
 		data_master_t *master = create_data_master(map_role, data_master_free_blocks);
-		fclient_t *fclient = create_fclient(rank, map_role->master_rank, CLIENT_LISTEN_TAG);
+		//fclient_t *fclient = create_fclient(rank, map_role->master_rank, CLIENT_LISTEN_TAG);
 
 		pthread_create(thread_data_master, NULL, data_master_init, master);
-		pthread_create(thread_client, NULL, fclient_run, fclient);
+		//pthread_create(thread_client, NULL, fclient_run, fclient);
 		pthread_join(*thread_data_master, NULL);
 		pthread_join(*thread_client, NULL);
-	} else if (map_role->type == DATA_SERVER) {
+	} else {
+		map_role->type = DATA_SERVER;
+		map_role->master_rank = 0;
+		map_role->group_size = size - 1;
+		map_role->rank = rank;
 		log_write(LOG_DEBUG, "ROLE DATA_SERVER and ID IS %d and master rank = %d", rank, map_role->master_rank);
 		usleep(10);
+		//register to master
 		c_d_register_t* re = get_register_cmd(rank, data_server_free_blocks, rank, net_name);
 		data_master_request_t *request = create_data_master_request(rank, map_role->master_rank, 169 + rank);
 		request->op->register_to_master(request, re);
@@ -78,10 +77,10 @@ int main(argc, argv)
 		zfree(re);
 
 		data_server_t *server = alloc_dataserver(data_server_free_blocks, rank);
-		fclient_t *fclient = create_fclient(rank, map_role->master_rank, CLIENT_LISTEN_TAG);
+		//fclient_t *fclient = create_fclient(rank, map_role->master_rank, CLIENT_LISTEN_TAG);
 
 		pthread_create(thread_data_server, NULL, dataserver_run, server);
-		pthread_create(thread_client, NULL, fclient_run, fclient);
+		//pthread_create(thread_client, NULL, fclient_run, fclient);
 		pthread_join(*thread_data_server, NULL);
 		pthread_join(*thread_client, NULL);
 	}
